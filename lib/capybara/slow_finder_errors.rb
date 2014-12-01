3288353424b2e07a314ca453bf96401e1a365331
Carol Nichols <cnichols@thinkthroughmath.com>
2014-11-30 21:56:05 -0500

    Be more lenient about args to be more compatible with capybara versions

--- lib/capybara/slow_finder_errors.rb
+++ lib/capybara/slow_finder_errors.rb
@@ -4,10 +4,11 @@ module Capybara
 
   module Node
     class Base
-      def synchronize_with_timeout_error(seconds=Capybara.default_wait_time, options = {}, &block)
+      def synchronize_with_timeout_error(*args, &block)
         start_time = Time.now
-        synchronize_without_timeout_error(seconds, options, &block)
+        synchronize_without_timeout_error(*args, &block)
       rescue Capybara::ElementNotFound => e
+        seconds = args.first || Capybara.default_wait_time
         if Time.now-start_time > seconds
           raise SlowFinderError, "Timeout reached while running a *waiting* Capybara finder...perhaps you wanted to return immediately? Use a non-waiting Capybara finder. More info: http://blog.codeship.com/faster-rails-tests?utm_source=gem_exception"
         end
