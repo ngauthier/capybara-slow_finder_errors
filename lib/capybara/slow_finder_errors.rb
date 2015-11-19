e8e588d5df01e384419a42d9a83132fc54822e3d
Clemens Kofler <clemens@railway.at>
2015-11-19 19:57:09 +0100

    Keep original exception and backtrace

--- lib/capybara/slow_finder_errors.rb
+++ lib/capybara/slow_finder_errors.rb
@@ -1,6 +1,6 @@
 require 'capybara'
 module Capybara
-  class SlowFinderError < CapybaraError; end
+  ADDITIONAL_MESSAGE = 'Timeout reached while running a *waiting* Capybara finder...perhaps you wanted to return immediately? Use a non-waiting Capybara finder. More info: http://blog.codeship.com/faster-rails-tests?utm_source=gem_exception'
 
   module Node
     class Base
@@ -9,10 +9,9 @@ module Capybara
         synchronize_without_timeout_error(*args, &block)
       rescue Capybara::ElementNotFound => e
         seconds = args.first || Capybara.default_max_wait_time
-        if seconds > 0 && Time.now-start_time > seconds
-          raise SlowFinderError, "Timeout reached while running a *waiting* Capybara finder...perhaps you wanted to return immediately? Use a non-waiting Capybara finder. More info: http://blog.codeship.com/faster-rails-tests?utm_source=gem_exception"
-        end
-        raise
+        raise e unless seconds > 0 && Time.now-start_time > seconds
+
+        raise e, "#{$!}\n\n#{ADDITIONAL_MESSAGE}", e.backtrace
       end
       alias_method :synchronize_without_timeout_error, :synchronize
       alias_method :synchronize, :synchronize_with_timeout_error
